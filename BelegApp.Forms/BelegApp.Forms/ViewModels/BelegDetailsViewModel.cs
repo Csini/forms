af121e5312f3c6c029319a50a51c6edd2723dd36
t-mxcom <github@t-mx.com>
2017-12-05 13:49:49 +0100

    supa

---
+++ BelegApp.Forms/BelegApp.Forms/ViewModels/BelegDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IO.Swagger.Model;
+
+namespace BelegApp.Forms.ViewModels
+{
+    class BelegDetailsViewModel
+    {
+
+        public BelegDetailsViewModel(Beleg beleg)
+        {
+
+        }
+    }
+}
