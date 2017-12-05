822da599c950be728a8ea2b1c2ab41aee29871c5
t-mxcom <github@t-mx.com>
2017-12-05 21:58:47 +0100

    Push it

---
+++ BelegApp.Forms/BelegApp.Forms/Utils/ImageServices.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+
+namespace BelegApp.Forms.Utils
+{
+    static class ImageServices
+    {
+        public static async Task<MediaFile> CaptureImage()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if ((!CrossMedia.Current.IsCameraAvailable) || (!CrossMedia.Current.IsTakePhotoSupported))
+            {
+                throw new InvalidOperationException();
+            }
+
+            StoreCameraMediaOptions mediaOptions = new StoreCameraMediaOptions()
+            {
+                Directory = "BelegApp",
+                Name = "beleg.jpg"
+            };
+            MediaFile mediaFile = await CrossMedia.Current.TakePhotoAsync(mediaOptions);
+
+            return mediaFile;
+        }
+
+        public static async Task<MediaFile> SelectImage()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                throw new InvalidOperationException();
+            }
+
+            PickMediaOptions mediaOptions = null; //  new PickMediaOptions();
+            MediaFile mediaFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
+
+            return mediaFile;
+        }
+    }
+}
