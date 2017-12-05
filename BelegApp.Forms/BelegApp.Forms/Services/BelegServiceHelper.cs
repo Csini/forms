6a7dde7bc53996faa5d563c7feac4c1263186fe4
jokster <jokster@gmx.at>
2017-12-05 19:21:38 +0100

    namespace BelegServiceHelper

--- BelegApp.Forms/BelegApp.Forms/Services/BelegServiceHelper.cs
+++ BelegApp.Forms/BelegApp.Forms/Services/BelegServiceHelper.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BelegApp.Forms.Utils
+namespace BelegApp.Forms.Services
 {
     public class BelegServiceHelper
     {
