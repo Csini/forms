7dcc7e7ae093dce66c67b77e0a5552f357c5a392
rlat <r7attus>
2017-12-05 20:41:38 +0100

    ExportBelege

--- BelegApp.Forms/BelegApp.Forms/Services/BelegServiceHelper.cs
+++ BelegApp.Forms/BelegApp.Forms/Services/BelegServiceHelper.cs
@@ -9,9 +9,34 @@ namespace BelegApp.Forms.Services
     public class BelegServiceHelper
     {
 
-        public Task<int> ExportBelege(Beleg[] belege)
+        public static async Task ExportBelege(Beleg[] belege)
         {
-            throw new NotImplementedException();
+            if (belege != null)
+            {
+                List<Beleg> belegList = new List<Beleg>(belege);
+                IEnumerable<Task> ExportTasksQuery = from beleg in belegList select ExportBeleg(beleg);
+
+                // Use ToArray to execute the query and start the download tasks.  
+                Task<int>[] downloadTasks = downloadTasksQuery.ToArray();
+                var tasks = new List<Task>();
+                foreach (Beleg beleg in belege)
+                {
+                    tasks.Add(ExportBeleg(beleg));
+                }
+                
+            }
+        }
+
+        static async Task ExportBeleg(Beleg beleg)
+        {
+            beleg.Belegnummer = await BelegService.CreateBeleg("forms", beleg);
+
+            if (!beleg.Belegnummer.HasValue)
+            {
+                throw new Exception("Vermisse Beleg-ID!");
+            }
+
+            await BelegService.SaveBelegImage("forms", beleg.Belegnummer.Value, beleg.Image);
         }
 
         public Task<int> RefreshStatus()
