c9e141a77fedada43032b136368aebfc163b3cd3
rlat <r7attus>
2017-12-05 19:18:49 +0100

    Verschoben

--- BelegApp.Forms/BelegApp.Forms/Services/BelegServiceHelper.cs
+++ BelegApp.Forms/BelegApp.Forms/Services/BelegServiceHelper.cs
@@ -3,14 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using BelegApp.Forms.Utils;
 
-namespace BelegApp.Forms.Utils
+namespace BelegApp.Forms.Services
 {
     public class BelegServiceHelper
     {
 
         public Task<int> ExportBelege(Beleg[] belege)
         {
+            if (belege != null)
+            {
+                foreach (Beleg beleg in belege)
+                {
+                    
+
+                }
+            }
             throw new NotImplementedException();
         }
 
